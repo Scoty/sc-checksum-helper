050c76b9876b79d81409bde4d2397ef1bc7e2241
antona <antona@safecharge.com>
2018-08-03 19:07:13 +0300

    FIX ME: We need ordered params for proper hashing - in java we are using LinkedHashMap

--- test/sc_checksum_helper_test.exs
+++ test/sc_checksum_helper_test.exs
@@ -2,7 +2,11 @@ defmodule ScChecksumHelperTest do
   use ExUnit.Case
   doctest ScChecksumHelper
 
-  test "greets the world" do
-    assert ScChecksumHelper.hello() == :world
+  test "valid checksum" do
+    sc_request = %ScRequest{secret_key: 123}
+    params = %{}
+    version = :v4
+
+    assert ScChecksumHelper.get_link!(sc_request, params, version) == "asd"
   end
 end
